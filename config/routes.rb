632ebf3fd428392a4e6a773ee0a2145020c21f37
t-miyamoto0809 <t.miyamoto0809@gmail.com>
2019-08-31 02:21:30 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
-  get 'users/:id' => 'users#show'
-  delete 'tweets/:id' => 'tweets#destroy'
-  get 'tweets/:id/edit' => 'tweets#edit'
-  patch 'tweets/:id' => 'tweets#update'
-  get 'tweets/:id' => 'tweets#show'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
